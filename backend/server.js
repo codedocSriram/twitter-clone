6d2bfc50b7ebbb99d8e9c3f463f4ed91e4349f1d
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-14 11:41:24 +0530

    Added cors and express.json middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRouter from "./routes/auth.router.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
