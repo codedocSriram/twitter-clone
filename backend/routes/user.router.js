dce8ac129cfd85b8120d3594c95aa18ea26fe24b
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-16 15:55:53 +0530

    Added endpoints for suggestedUser and update user in user router file

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -2,14 +2,15 @@ import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
 import {
     followUnfollowUser,
+    getSuggestedUsers,
     getUserProfile,
-    updateUserProfile,
+    updateUser,
 } from "../controllers/user.controller.js";
 const userRouter = express.Router();
 
 userRouter.get("/profile/:username", protectRoute, getUserProfile);
-// userRouter.post("/suggested", protectRoute, getUserProfile);
+userRouter.post("/suggested", protectRoute, getSuggestedUsers);
 userRouter.post("/follow/:id", protectRoute, followUnfollowUser);
-userRouter.post("/update", protectRoute, updateUserProfile);
+userRouter.post("/update", protectRoute, updateUser);
 
 export default userRouter;
