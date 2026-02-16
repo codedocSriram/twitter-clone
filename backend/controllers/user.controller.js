de51c35e76131d5c21a0ce48cad2f54c9590bc9b
Sriram Annadurai <mailsriram98@gmail.com>
2026-02-16 16:06:20 +0530

    cleared a typo

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import Notification from "../models/notification.models.js";
+import Notification from "../models/notification.model.js";
 import bcrypt from "bcryptjs";
 import { v2 as cloudinary } from "cloudinary";
 export const getUserProfile = async (req, res) => {
